6f5d47b9beea394095e886316282ce11aff8a512
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2023-03-20 14:39:00 -0500

    Implementing task local storage for HPX backend

--- flecsi/run/context.hh
+++ flecsi/run/context.hh
@@ -406,7 +406,7 @@ public:
 
 protected:
   // Invoke initialization callbacks.
-  // Call from hiding function in derived classses.
+  // Call from hiding function in derived classes.
   void start() {
     for(auto ro : init_registry)
       ro();
@@ -514,12 +514,18 @@ private:
 struct task_local_base {
   struct guard {
     guard() {
-      for(auto * p : all)
-        p->emplace();
+      if(!all.empty()) {
+        all[0]->create_storage();
+        for(auto * p : all)
+          p->emplace();
+      }
     }
     ~guard() {
-      for(auto * p : all)
-        p->reset();
+      if(!all.empty()) {
+        for(auto * p : all)
+          p->reset();
+        all[0]->reset_storage();
+      }
     }
   };
 
@@ -538,6 +544,8 @@ private:
 
   virtual void emplace() = 0;
   virtual void reset() noexcept = 0;
+  virtual void create_storage() {}
+  virtual void reset_storage() noexcept {}
 };
 
 /// \endcond
