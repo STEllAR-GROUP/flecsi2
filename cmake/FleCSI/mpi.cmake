b1f394fc7f01f8ce8e9a7df08233af5a00731e21
Davis Herring <herring@lanl.gov>
2023-12-06 19:35:11 -0700

    Factor out MSVC defines

--- cmake/FleCSI/mpi.cmake
+++ cmake/FleCSI/mpi.cmake
@@ -11,10 +11,4 @@ macro(flecsi_enable_mpi target ENABLE_CXX_BINDINGS)
   endif()
   target_link_libraries(${target} PUBLIC MPI::MPI_CXX)
 
-  if(MSVC)
-    target_compile_definitions(${target} PUBLIC
-      _CRT_SECURE_NO_WARNINGS
-      NOMINMAX)
-  endif()
-
 endmacro()
